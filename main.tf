7be764e092e528d9e498d9ca780baa596a83ed75
V@ibhav <V@gmail.com>
2021-07-24 13:11:40 +0530

    region is uswest1 now

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
    }
 
 provider "aws" {
-       #region = "us-east-1" 
+       region = "us-west-1" 
         profile = "default"
 	}
 
 resource "aws_instance" "Raminfra" {
 	ami = "ami-0dc2d3e4c0f9ebd18" 
 	instance_type = "t2.micro"
-  count = 3
+  #count = 3
 }
